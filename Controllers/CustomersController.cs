a337c426cce9bdefb1a0e46a967d6b3ac0eba3b9
Abdul <Abdul@DESKTOP-AONUFGH>
2022-02-18 10:15:46 +0300

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+
+        public ActionResult AllCustomers()
+        {
+
+            var customers = new List<Customer>()
+            {
+                new Customer { Id = 1, Name = "Abdul"},
+                new Customer { Id = 2, Name = "Mary"}
+            };
+            return View(customers);
+        }
+
+
+        public ActionResult Customer(int id, string name)
+        {
+            var customer = new Customer { Id = id, Name = name };
+            return View(customer);
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer> {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name ="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Cutomers = customers
+            };
+
+            return View(viewModel);
+        }
+
+
+        public ActionResult AllMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {Name = "Shrek"},
+                new Movie {Name ="Wall-E"}
+            };
+            return View(movies);
+        }
+
+
+
+    }
+}
